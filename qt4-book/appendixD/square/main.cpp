c54db28c1402f29bb8080c77a2a35549963d87b3
万泽 <a358003542@gmail.com>
2014-12-05 20:33:58 +0800

    2014-12-05 20:33

---
+++ qt4-book/appendixD/square/main.cpp
@@ -0,0 +1,16 @@
+#include <cstdlib>
+#include <iostream>
+
+double square(double);
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2) {
+        std::cerr << "Usage: square <number>" << std::endl;
+        return 1;
+    }
+    double n = std::strtod(argv[1], 0);
+    std::cout << "The square of " << argv[1] << " is "
+        << square(n) << std::endl;
+    return 0;
+}
